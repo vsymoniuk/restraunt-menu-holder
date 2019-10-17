eb3e74b04e2fa5bb4f32e09f18a33e33907ce10c
leakerman <izogid42@gmail.com>
2019-10-17 20:56:50 +0300

    try to host2

--- app.js
+++ app.js
@@ -4,7 +4,6 @@ const express = require('express')
 const path = require('path')
 const app = express()
 
-const links = require('./links')
 
 const authRoutes = require('./routes/auth')
 const categoryRoutes = require('./routes/category')
@@ -12,22 +11,33 @@ const orderRoutes = require('./routes/order')
 const positionRoutes = require('./routes/position')
 const tableRoutes = require('./routes/table')
 
+const links = require('./links')
+let mongoLink
+
+if (process.env.NODE_ENV === 'production') {
+  mongoLink = process.env.MONGO_URI
+} else {
+  mongoLink = links.mongoURI
+}
+
+mongoose.connect(mongoLink)
+  .then(() => console.log('Mongo connected.'))
+  .catch(error => console.log(error))
+
 mongoose.set('useNewUrlParser', true);
 mongoose.set('useFindAndModify', false);
 mongoose.set('useCreateIndex', true);
 mongoose.set('useUnifiedTopology', true);
-
-mongoose.connect(links.mongoURI)
-  .then(() => console.log('Mongo connected.'))
-  .catch(error => console.log(error))
 
 app.use(bodyParser.urlencoded({
   extended: true
 }))
 app.use(bodyParser.json())
 
+
 app.use(require('morgan')('dev'))
 app.use(require('cors')())
+
 
 app.use('/api/auth', authRoutes)
 app.use('/api/category', categoryRoutes)
@@ -50,7 +60,6 @@ if (process.env.NODE_ENV === 'production') {
       )
     )
   })
-
 }
 
 module.exports = app
